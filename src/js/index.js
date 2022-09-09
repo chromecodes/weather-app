a68b1ccab86bc62777bed89ab3783255ac717b28
chromecodes <hameedhussain782@gmail.com>
2022-09-09 23:18:01 +0530

    updated file

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import '../css/normalize.css';
+import '../css/style.css';
+
+import { apiData } from './Data';
+
+console.log(
+    apiData.crutWthrFetch('Amsterdam','metric'));
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    
     module: {
         rules: [
             {
