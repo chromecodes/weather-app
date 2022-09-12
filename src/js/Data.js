0e088c0518dc5be5ab1c85b8e67e2f51a66a0aae
chromecodes <hameedhussain782@gmail.com>
2022-09-12 23:08:27 +0530

    updated files

--- src/js/Data.js
+++ src/js/Data.js
@@ -1,9 +1,28 @@
 export const apiData = (()=>{
 
     async function crutWthrFetch (cityname, unit) {
-        console.log(3);
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&APPID=8451ef5d5c10364ca6777b6b4a4ef99f&units=${unit}`, {mode: 'cors'} ) ;
+        const apiKey ='8451ef5d5c10364ca6777b6b4a4ef99f'
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&APPID=${apiKey}&units=${unit}`, {mode: 'cors'} ) ;
         const crutWthrData = await response.json();
+        console.log(crutWthrData);
+        const forcastResponse = await fetch (`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${crutWthrData.coord.lat}&lon=${crutWthrData.coord.lon}&cnt={7}&appid=${apiKey}`, {mode: 'cors'})
+        const forcastData = await forcastResponse.json();
+        console.log(forcastData);
+
+        
+
+        // const getTime = (timezone) => {
+        //     const localTime = new Date().getTime()
+        //     const localOffset = new Date().getTimezoneOffset() * 60000
+        //     const currentUtcTime = localOffset + localTime + 86400
+        //     const cityOffset = currentUtcTime + 1000 * timezone
+        //     const cityTime = new Date(cityOffset).toTimeString().split(' ')
+        //     const cityTime2 = new Date(cityOffset)
+        //     return { time: cityTime[0], full:cityTime2 }
+        //   }
+        //   console.log(getTime(data.timezone));  
+
+
         return  crutWthrData;
     }
     
--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,15 @@ import {listeners} from './listener'
 
 
 listeners()
-let t2 = new Date().getUTCSeconds
-console.log(t2);
+// let t2 = new Date.UTC()
+// console.log(t2);
+
+// export const getTime = (timezone) => {
+//     const localTime = new Date().getTime()
+//     const localOffset = new Date().getTimezoneOffset() * 60000
+//     const currentUtcTime = localOffset + localTime
+//     const cityOffset = currentUtcTime + 1000 * timezone
+//     const cityTime = new Date(cityOffset).toTimeString().split(' ')
+//     return { time: cityTime[0] }
+//   }
+  
